60f19d663e6ee3bd4f5c99e80fc013cc3e548f60
g-dailey <gulafroz.rezai@gmail.com>
2022-09-30 22:47:24 -0700

    Added Resized Update Profile Picture Feature using Pillow Library

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_login import current_user
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, EmailField, ValidationError
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 import email_validator
@@ -39,6 +40,7 @@ class LoginForm(FlaskForm):
 class UpdateAccountForm(FlaskForm):
   username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
   email = StringField('Email', validators=[DataRequired(), Email()])
+  picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
   submit = SubmitField('Update')
 
   def validate_username(self, username):
