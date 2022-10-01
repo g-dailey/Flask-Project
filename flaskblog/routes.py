0a873f86bc8d81ce36dfcd39d944fa389aab1e0c
g-dailey <gulafroz.rezai@gmail.com>
2022-10-01 11:39:07 -0700

    Added Update Post Feature

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,5 +1,5 @@
 
-from flask import render_template, redirect, url_for, flash, redirect, request
+from flask import render_template, redirect, url_for, flash, redirect, request, abort
 from flaskblog import app, db, bcrypt
 import os
 import secrets
@@ -109,7 +109,7 @@ def new_post():
     flash('Your Post has been created!', 'success')
     return redirect(url_for('home'))
 
-  return render_template('create_post.html', title="New Post", form=form)
+  return render_template('create_post.html', title="New Post", form=form, legend="New Post")
 
 
 @app.route("/post/<int:post_id>")
@@ -117,3 +117,23 @@ def new_post():
 def post(post_id):
   post = Post.query.get_or_404(post_id)
   return render_template('post.html', title=post.title, post = post)
+
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def udpate_post(post_id):
+  post = Post.query.get_or_404(post_id)
+  if post.author != current_user:
+    abort(403)
+  form = PostForm()
+  if form.validate_on_submit():
+    post.title = form.title.data
+    post.content = form.content.data
+    db.session.commit()
+    flash('Your post has been updated', 'success')
+    return redirect(url_for('post', post_id=post.id))
+  elif request.method == 'GET':
+    form.title.data = post.title
+    form.content.data = post.content
+  return render_template('create_post.html', title="Update Post",
+                        form=form, legend="Update Post")
